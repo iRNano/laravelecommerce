d06fef84e503728a67ab9d2ec15dfddbeae57ab4
Adrian Valdepenas <valdepenas.adrian@gmail.comgit config --global user.email valdepenas.adrian@gmail.com>
2020-03-03 16:24:16 +0800

    added username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,9 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function username(){
+        return 'username';
+    }
     /**
      * Where to redirect users after login.
      *
